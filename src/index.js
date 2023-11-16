8fa3f04826f40db8cf91ce432225eb5584bd12bb
NhatTan1212 <honhattan121@gmail.com>
2023-11-16 08:37:11 +0700

    update route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,7 +9,7 @@ import Provider from './store/Provider';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename={'/Phoenix-technology'}>
+    <Router>
       <Provider>
         <App />
       </Provider>
