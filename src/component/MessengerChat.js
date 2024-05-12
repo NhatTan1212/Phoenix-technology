1764ac8c76b9360b0dffda05e91cad4fc6389220
NhatTan1212 <honhattan121@gmail.com>
2024-05-12 21:00:40 +0700

    test instagram chat

--- src/component/MessengerChat.js
+++ src/component/MessengerChat.js
@@ -1,21 +1,16 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import ElfisghtWidget from 'react-elfsight-widget';
 
 function MessengerChat() {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://static.elfsight.com/platform/platform.js';
-    script.setAttribute('data-use-service-core', '');
-    script.defer = true;
-
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
   return (
-    <div className="elfsight-app-ae30d92d-90ff-443e-a17f-f34311108468" data-elfsight-app-lazy></div>
+    <div>
+      <ElfisghtWidget
+        src="https://static.elfsight.com/platform/platform.js"
+        data-use-service-core
+        data-elfsight-app-lazy
+        appId="ae30d92d-90ff-443e-a17f-f34311108468"
+      />
+    </div>
   );
 }
 
