64f5e634a80e93b4721727168270a53af890152e
NhatTan1212 <116484948+NhatTan1212@users.noreply.github.com>
2024-05-12 20:30:10 +0700

    Update MessengerChat.js

--- src/component/MessengerChat.js
+++ src/component/MessengerChat.js
@@ -1,15 +1,22 @@
-import React, { Component } from 'react';
-import { FacebookProvider, CustomChat } from 'react-facebook';
+import React, { useEffect } from 'react';
 
-export default class MessengerChat extends Component {
-    render() {
-        return (
-            <FacebookProvider appId="924883292758885" chatSupport>
-                <CustomChat
-                    pageId="277034358822420"
-                    themeColor="#df1b21"
-                    minimized={true} />
-            </FacebookProvider>
-        );
-    }
+function MessengerChat() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://static.elfsight.com/platform/platform.js';
+    script.setAttribute('data-use-service-core', '');
+    script.defer = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div className="elfsight-app-ae30d92d-90ff-443e-a17f-f34311108468" data-elfsight-app-lazy></div>
+  );
 }
+
+export default MessengerChat;
